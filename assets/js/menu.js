5758d2e9eb3a884925744a2e0d7fa88b03fddf6e
noahLogan <95936123+noahLogan@users.noreply.github.com>
2024-10-24 03:22:26 -0700

    Delete assets directory

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menuIcon = document.getElementById('menu-icon');
-    const menu = document.getElementById('menu');
-    
-    // Cambia entre la clase "change" para el ícono y "open" para el menú
-    menuIcon.classList.toggle('change');
-    menu.classList.toggle('open');
-  }
